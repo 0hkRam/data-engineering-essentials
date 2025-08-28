148b20d788dd936aaf996dbbc42313ee0a48059c
Hareram kumar <145317295+0hkRam@users.noreply.github.com>
2025-08-28 20:58:54 +0530

    Update 165 Solutions for filtering and aggregations.sql

--- 025 SQL Essentials for Data Engineers/085 Solutions for Basic SQL Queries/165 Solutions for filtering and aggregations.sql
+++ 025 SQL Essentials for Data Engineers/085 Solutions for Basic SQL Queries/165 Solutions for filtering and aggregations.sql
@@ -12,7 +12,7 @@ WHERE course_name LIKE '%Python%' OR course_name LIKE '%Scala%';
 -- * Get count of courses by `course_status`. The output should contain `course_status` and `course_count`.
 SELECT course_status,
     count(*) AS course_count
-FROM courses
+FROM courses 
 GROUP BY 1;
 
 -- * Get count of `published` courses by `course_author`. The output should contain `course_author` and `course_count`.
@@ -21,13 +21,13 @@ SELECT course_author,
 FROM courses
 WHERE course_status = 'published'
 GROUP BY 1;
-
+ 
 -- * Get all the details of `Python` or `Scala` related courses in `draft` status.
 SELECT *
 FROM courses
 WHERE (course_name LIKE '%Python%' OR course_name LIKE '%Scala%')
     AND course_status = 'draft';
-
+ 
 -- * Get the author and count where the author have more than **one published** course. The output should contain `course_author` and `course_count`.
 SELECT course_author,
     count(*) AS course_count
